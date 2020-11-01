dc1f7418b32d2bee48bbc59aa7d82bee9d072763
bryancheng66 <bryancheng66@gmail.com>
2020-10-31 21:30:11 -0400

    Add method distanceTo

--- Point.java
+++ Point.java
@@ -18,6 +18,10 @@ public class Point {
 	public double getY(){
 		return y;
 	}	
+
+	public double distanceTo(Point other){
+		return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+	}
 }
 
 
--- Test.java
+++ Test.java
@@ -2,7 +2,12 @@ public class Test {
 	public static void main(String[] Args){
 		Point p1 = new Point(10,19);
 		Point p2 = new Point(p1);
+		Point p3 = new Point(0, 0);
+		Point p4 = new Point(1, 1);
+
 		System.out.println(p1.getX() + ", " + p1.getY());
 		System.out.println(p2.getX() + ", " + p2.getY());
-	}
+		System.out.println(p1.distanceTo(p3));
+		System.out.println(p3.distanceTo(p4));
+	}	
 }
